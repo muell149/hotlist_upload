e6bdfecc21738f4fde817276a91e5d88a5831a54
charles mueller <cmueller@gogoair.com>
2019-10-18 21:19:37 +0000

    removing quote additions, killing whitespace, better printouts

--- onedrive_to_s3.sh
+++ onedrive_to_s3.sh
@@ -1,25 +1,39 @@
-#copy from onedrive
+#!/bin/bash
+
+### usage: ./onedrive_to_s3.sh
+
+echo 'copying from onedrive...'
 rclone copy onedrive:Charlie_LRU_NFF_Failure_Prediction.xlsx .
 
-#convert to csv
+echo ''
+echo 'converting xlsx to csv...'
 in2csv Charlie_LRU_NFF_Failure_Prediction.xlsx > test.csv
 
-#add quotes and remove header:
-csvformat -K 1 -q "'" -U 1 test.csv > data.csv
+echo ''
+echo 'removing header....'
+csvformat -K 1 test.csv > data.csv
 
-#remove empty strings:
-sed -i 's/""//g' data.csv
+echo ''
+echo 'removing any whitespace...'
+sed -i 's/ //g' data.csv
 
-#finally, copy to aws s3 bucket:
-#aws s3 cp --sse AES256 data.csv s3://gogo-udp-ds-prod/data/ds/reliability_hotlist/data.csv
+echo ''
+echo 'copying to s3 bucket...'
+aws s3 cp --sse AES256 data.csv s3://gogo-udp-ds-prod/data/ds/reliability_hotlist/data.csv
 
-#print what was uploaded to s3
+echo ''
+echo 'contents uploaded to s3:'
 cat data.csv
 
-#print header
+echo ''
+echo 'header:'
 head -1 test.csv
 
-#cleanup
+echo ''
+echo 'cleaning up (deleting) copied files...'
 rm Charlie_LRU_NFF_Failure_Prediction.xlsx
 rm test.csv
 rm data.csv
+
+echo ''
+echo 'done.'
